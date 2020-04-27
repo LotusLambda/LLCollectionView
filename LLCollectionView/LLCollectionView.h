f0b9c6de11b2180951a4b7d0a276b4a8bcdcd0d4
Aron Balog <aronbalog@gmail.com>
2020-04-27 03:56:19 +0200

    Initial Commit

---
+++ LLCollectionView/LLCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  LLCollectionView.h
+//  LLCollectionView
+//
+//  Created by Aron Balog on 27/04/2020.
+//  Copyright © 2020 Lotus Lambda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LLCollectionView.
+FOUNDATION_EXPORT double LLCollectionViewVersionNumber;
+
+//! Project version string for LLCollectionView.
+FOUNDATION_EXPORT const unsigned char LLCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LLCollectionView/PublicHeader.h>
+
+
